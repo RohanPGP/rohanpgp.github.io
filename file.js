7465de272d83f67b2b5e084e2aad9cb0424a8e03
RohanPGP <159392534+RohanPGP@users.noreply.github.com>
2024-12-11 19:41:44 -0600

    Create file.js

---
+++ file.js
@@ -0,0 +1,24 @@
+    let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let slides = document.querySelectorAll(".slide");
+  slides.forEach(slide => slide.style.display = "none");
+
+  slideIndex++;
+  if (slideIndex > slides.length) { slideIndex = 1; }
+  slides[slideIndex - 1].style.display = "block";
+
+  setTimeout(showSlides, 3000); // Change image every 3 seconds
+}
+
+function changeSlide(n) {
+  let slides = document.querySelectorAll(".slide");
+  slides[slideIndex - 1].style.display = "none";
+
+  slideIndex += n;
+  if (slideIndex > slides.length) { slideIndex = 1; }
+  if (slideIndex < 1) { slideIndex = slides.length; }
+
+  slides[slideIndex - 1].style.display = "block";
+}
